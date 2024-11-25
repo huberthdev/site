f4f3c97ebf6641106fa54f8855d8859e7e4d17b6
tibe <huberth.santanaup@outlook.com>
2024-11-25 11:45:37 -0300

    nova conexao postgre

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, redirect, jsonify, render_template
 from flask_cors import CORS
-import sqlite3
+import psycopg2
 import os
 import logging
+from dotenv import load_dotenv
+
+# Carrega variáveis de ambiente do arquivo .env
+load_dotenv()
 
 # Configuração de logs
 logging.basicConfig(level=logging.INFO)
@@ -10,47 +14,41 @@ logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://www.servhidel.com.br"}})  # Restrição de CORS
 
+# Configurações do PostgreSQL
+DB_CONFIG = {
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
+}
+
+def get_db_connection():
+    """Estabelece uma conexão com o banco PostgreSQL."""
+    return psycopg2.connect(
+        dbname=DB_CONFIG['dbname'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        host=DB_CONFIG['host'],
+        port=DB_CONFIG['port']
+    )
+
 @app.before_request
 def handle_redirects():
     """Redireciona para www e força HTTPS."""
-    # Redireciona para www.servhidel.com.br
     if request.host == 'servhidel.com.br':
         return redirect('https://www.servhidel.com.br' + request.full_path, code=301)
-    
-    # Garante HTTPS
     if not request.is_secure and 'HEROKU' in os.environ:
         return redirect(request.url.replace("http://", "https://"), code=301)
 
-
-# Função para inicializar o banco de dados SQLite
-def init_sqlite_db():
-    conn = sqlite3.connect('database.db')
-    logging.info("Base de dados aberta com sucesso")
-    conn.execute('''CREATE TABLE IF NOT EXISTS cotacao (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        phone TEXT NOT NULL,
-        description TEXT NOT NULL
-    )''')
-    logging.info("Tabela criada ou já existente")
-    conn.close()
-
-# Inicializa o banco de dados ao iniciar o servidor
-init_sqlite_db()
-
 @app.route('/')
 def home():
-    """Rota para a página inicial."""
     return render_template('index.html')
 
 @app.route('/api/cotacao', methods=['POST'])
 def criar_cotacao():
-    """Rota para criar uma nova cotação."""
     try:
         data = request.get_json()
-
-        # Validações básicas
         if not all(key in data for key in ['name', 'email', 'phone', 'description']):
             return jsonify({"error": "Faltam campos obrigatórios"}), 400
 
@@ -58,18 +56,20 @@ def criar_cotacao():
         email = data['email'].lower()
         phone = data['phone']
         description = data['description'].upper()
+        status = data.get('status', 0)  # Usando o valor padrão 0 para status se não informado
 
-        # Conexão com o banco de dados
-        conn = sqlite3.connect('database.db')
+        conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute("""INSERT INTO cotacao (name, email, phone, description)
-                           VALUES (?, ?, ?, ?)""", (name, email, phone, description))
+        cursor.execute("""
+            INSERT INTO cotacao (name, email, phone, description, status)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (name, email, phone, description, status))
         conn.commit()
         cursor.close()
         conn.close()
 
         return jsonify({"message": "Orçamento solicitado com sucesso!\n\nEntraremos em contato em breve."}), 201
-    except sqlite3.Error as e:
+    except psycopg2.Error as e:
         logging.error(f"Erro no banco de dados: {e}")
         return jsonify({"error": "Erro no banco de dados"}), 500
     except Exception as e:
@@ -78,24 +78,20 @@ def criar_cotacao():
 
 @app.route('/api/cotacoes', methods=['GET'])
 def listar_cotacoes():
-    """Rota para listar todas as cotações."""
     try:
-        conn = sqlite3.connect('database.db')
+        conn = get_db_connection()
         cursor = conn.cursor()
-
         cursor.execute("SELECT * FROM cotacao")
         rows = cursor.fetchall()
-
         cotacoes = [
-            {"id": row[0], "name": row[1], "email": row[2], "phone": row[3], "description": row[4]}
+            {"id": row[0], "name": row[1], "email": row[2], "phone": row[3], "description": row[4], "status": row[5]}
             for row in rows
         ]
-
         cursor.close()
         conn.close()
 
         return jsonify(cotacoes), 200
-    except sqlite3.Error as e:
+    except psycopg2.Error as e:
         logging.error(f"Erro no banco de dados: {e}")
         return jsonify({"error": "Erro no banco de dados"}), 500
     except Exception as e:
@@ -104,16 +100,15 @@ def listar_cotacoes():
 
 @app.route('/cotacoes')
 def cotacoes():
-    """Rota para renderizar a página de cotações."""
     try:
-        conn = sqlite3.connect('database.db')
+        conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM cotacao")
         cotacoes = cursor.fetchall()
         conn.close()
 
         return render_template('cotacoes.html', cotacoes=cotacoes)
-    except sqlite3.Error as e:
+    except psycopg2.Error as e:
         logging.error(f"Erro no banco de dados: {e}")
         return render_template('error.html', error="Erro ao acessar o banco de dados")
     except Exception as e:
@@ -121,5 +116,5 @@ def cotacoes():
         return render_template('error.html', error=str(e))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Porta padrão para Heroku
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
